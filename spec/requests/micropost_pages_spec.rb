5d510c8e26d012c3b158fd07cbfa7479aafde9c8
misato-okaneya <misato-okaneya@cookpad.com>
2014-08-13 18:22:58 +0900

    Add user following

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -20,6 +20,14 @@ describe "MicropostPages" do
         before { click_button "Post" }
         it { should have_content('error') }
       end
+
+      describe "micropost counts" do
+        it "should not increment micropost counts" do
+          page.should have_content('0 microposts')
+          click_button "Post"
+          page.should_not have_content('1 micropost')
+        end
+      end
     end
 
     describe "with valid information" do
@@ -27,6 +35,17 @@ describe "MicropostPages" do
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
+
+      describe "micropost counts" do
+        it "should increment micropost counts" do
+          page.should have_content('0 microposts')
+          click_button "Post"
+          page.should have_content('1 micropost')
+          fill_in 'micropost_content', with: "Lorem ipsum"
+          click_button "Post"
+          page.should have_content('2 microposts')
+        end
       end
     end
   end
@@ -39,6 +58,17 @@ describe "MicropostPages" do
 
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+
+    describe "as incorrect user" do
+      let(:another_user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: another_user)
+        visit user_path(another_user) 
+      end
+      it "should not have delete link" do
+        page.should_not have_link('delete')
       end
     end
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
   subject { page }
 
@@ -13,12 +13,15 @@ describe "UserPages" do
 
     it { should have_title('All users') }
     it { should have_content('All users') }
+    it { should_not have_selector('div.pagination') }
+    it { should_not have_content('Next') }
 
     describe "pagination" do
+
       before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
-
       it { should have_selector('div.pagination') }
+      it { should have_content('Next') }
 
       it "should list each user" do
         User.paginate(page: 1).each do |user|
@@ -167,6 +170,84 @@ describe "UserPages" do
         patch user_path(user), params
       end
       specify { expect(user.reload).not_to be_admin }
+    end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
+    end
+  end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
     end
   end
 end
